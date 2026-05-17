5a53bac5a595b34847328e795c28a6c05fb051c3
ayomikuolarewaju <almaroofolarewaju@gmail.com>
2026-05-17 10:20:44 +0100

    correct config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
+export default nextConfig;
 
-export default nextConfig
+
